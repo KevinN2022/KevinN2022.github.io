17d0c583aa07ea1f15e093422bf357192fc201ae
KevinN2022 <knguyenscv@gmail.com>
2024-06-02 09:40:57 -0400

    main

--- v2.3/public/beamer-lib/js/config.js
+++
@@ -1,8 +0,0 @@
-function Config()
-{
-    this.rootUrl = "http://167.71.177.11/the-board-room/v5/";
-    this.authUrl = this.rootUrl + "/auth/";
-    this.apiUrl = this.rootUrl + "/api/";
-    this.dataUrl = this.rootUrl + "/data/";
-    this.openAIUrl = this.rootUrl + "/open-ai/";
-}
